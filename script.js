0c87b7b9bd0cda82d864a6c9afd20fedfa7f83c7
Maria Clara <129561798+maariaclaara@users.noreply.github.com>
2023-04-08 15:59:57 -0300

    JS com virar cartas e checar par.

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 /*inicio do jogo*/
 
-let numberCards = Number ( prompt('Insira um número par entre 4 e 14.') );
+let numberCards = Number ( prompt('Quantas cartas você quer? (Número par entre 4 e 14)') );
 
 while (( numberCards < 4 || numberCards > 14 || (numberCards % 2) !== 0)){
-    numberCards = Number ( prompt('Insira um número par entre 4 e 14.') );
+    numberCards = Number ( prompt('Quantas cartas você quer? (Número par entre 4 e 14)') );
 }
 
 
 /*distribuir cartas*/
 
-const imgCards = [1,1,2,2,3,3,4,4,5,5,6,6,7,7];
+const imgCards = [
+"./imagem/carta1.gif" ,
+"./imagem/carta1.gif" ,
+"./imagem/carta2.gif" ,
+"./imagem/carta2.gif" ,
+"./imagem/carta3.gif" ,
+"./imagem/carta3.gif" ,
+"./imagem/carta4.gif" ,
+"./imagem/carta4.gif" ,
+"./imagem/carta5.gif" ,
+"./imagem/carta5.gif" ,
+"./imagem/carta6.gif" ,
+"./imagem/carta6.gif" ,
+"./imagem/carta7.gif" ,
+"./imagem/carta7.gif" ,
+];
 
 const cards = [];
 
@@ -31,16 +46,76 @@ function addCards(){
         containerCards.innerHTML = containerCards.innerHTML + `
             <div data-test="card" onclick="checkCard(this)" class="card">
                 <div class="front-face face">
-                    <img data-test="face-down-image" src="./imagem/back.png" class="img"/>
+                    <img data-test="face-down-image" src="./imagem/back.png" class="img" />
                 </div> 
                 <div class="back-face face">
-                    <img data-test="face-up-image" src="./imagem/${cards[i]}.gif" class="img"/>
+                    <img data-test="face-up-image" src="${cards[i]}" class="img" />
                 </div> 
             </div> ` ;
     }    
 }
 
 addCards();
+
+
+/*igualdade e diferença do par de cartas*/
+
+let moves = 0;
+let pair = 0;
+let faceCards = 0;
+let imgCard = "";
+
+function checkPair (image, card ) {
+
+    if (image.querySelector('img').src === card.querySelector('back-face>img').src){
+        
+        pair++;
+        faceCards = 0;
+        imgCard = "";
+
+    } else if (image.querySelector('image').src !== card.querySelector('back-face>img').src){
+    
+        imgCard = "";
+        image.parentElement.classList.remove('turn');
+        card.parentElement.classList.remove('turn');
+        faceCards = 0;
+    
+    } if (pair === (numberCards / 2)){
+        prompt ('Você ganhou em $(moves) jogadas!')
+    }
+}
+
+
+/*virar carta*/
+
+function checkCard(click){
+
+    if ((click.classList.contains('turn') === false) && faceCards === 0){
+        click.classList.add('turn');
+        imgCard = click.querySelector('.back-face');
+        moves++;
+        faceCards++;
+        
+    } else if ((click.classList.contains('turn') === false) && faceCards === 1){
+        click.classList.add('turn');
+        moves++;
+        faceCards++;
+        setTimeout(checkPair, 1000, imgCard, click);
+    } 
+}
+
+
+
+
+
+
+
+
+
+
+   
+
+
 
 
 
